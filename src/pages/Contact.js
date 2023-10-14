426b7bc40ea32561bd31d6e2352159ad0c79736e
raihven <raihven@protonmail.com>
2023-10-14 12:49:16 -0400

    mostly typos, change properties used

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,37 +1,34 @@
-import React from 'react'
-import contactLeft from "../assets/contactLeft.jpg"
-import "../styles/Contact.css";
+import React from 'react';
+import contactImage from '../assets/contactLeft.jpg';
+import '../styles/Contact.css';
 
 function Contact() {
   return (
     <div className="contact">
-    <div
+      <div
         className="contactLeft"
-        stlye={{ backgroundImage: `url(${contactLeft})` }}
-    ></div>
-    <div className="contactRight">
+        style={{ backgroundImage: `url(${contactImage})` }}
+      ></div>
+      <div className="contactRight">
         <h1> Contact Us </h1>
 
-    <form id="contact-form" method="POST">
-        <label htmlFor="name">Full Name</label>
-        <input name="text" placeholder="Input Full Name" type="text" />
-        <label htmlFor="email">Email</label>
-        <input name="email" placeholder="Input your email" type="email" />
-        <label htmlFor="messsage">Message</label>
-        <textarea 
-            rows="5" 
-            placeholder="Please, Let us know how we fucked up." 
-            name="message" 
+        <form id="contact-form" method="POST">
+          <label htmlFor="name">Full Name</label>
+          <input name="text" placeholder="Input Full Name" type="text" />
+          <label htmlFor="email">Email</label>
+          <input name="email" placeholder="Input your email" type="email" />
+          <label htmlFor="messsage">Message</label>
+          <textarea
+            rows="5"
+            placeholder="Please, Let us know how we fucked up."
+            name="message"
             required
-        >  
-        </textarea> 
-        <button type="submit"> Enviar Message </button>
-        </form> 
-        </div> 
-        </div> 
-        ); 
-    }
-
+          ></textarea>
+          <button type="submit"> Enviar Message </button>
+        </form>
+      </div>
+    </div>
+  );
+}
 
 export default Contact;
-
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -6,7 +6,7 @@ import "../styles/Menu.css";
 function Menu() {
   return (
     <div className="menu">
-      <h1 clasName="menuTitle">Products</h1>
+      <h1 className="menuTitle">Products</h1>
       
       <div className="menuList">
         {MenuList.map((menuItem, key) => {
